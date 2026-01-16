6f842d6ab3817d97725674daa48b5764433534bc
Aaron Rowlands <aaron-s@gmx.net>
2026-01-16 18:10:05 +0800

    Refactor day 3 solution

--- day3.py
+++ day3.py
@@ -6,32 +6,36 @@ def main():
     disableString = "don't()"
     mulPattern = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
 
-    expressionList = []
-    enablePattern  = re.escape(enableString)
-    disablePattern = re.escape(disableString)
-    validExpressionPattern = f"{mulPattern}|{enablePattern}|{disablePattern}"
-
     with open(sys.argv[1]) as inputfile:
         inputString = inputfile.read()
 
-    expressionList += re.findall(validExpressionPattern, inputString)
-
-    multiplicationSum = 0
-    currentlyEnabled = True
-    for expression in expressionList:
-        if expression == enableString:
-            currentlyEnabled = True
-        elif expression == disableString:
-            currentlyEnabled = False
-        else:
-            # multiplication expression
-            if currentlyEnabled:
-                multiplicationSum += evaluate(expression)
+    multiplicationSum = evaluateString(inputString, mulPattern, (enableString, disableString))
 
     # TODO: Restore part 1 solution calculation
     print(f"Sum of all multiplication instructions: {multiplicationSum}")
 
-def evaluate(expression):
+def evaluateString(input, mulPattern, enableAndDisableStr):
+    enableStr, disableStr = enableAndDisableStr
+
+    patternList = [mulPattern, re.escape(enableStr), re.escape(disableStr)]
+    combinedPattern = "|".join(patternList)
+
+    expressionList = re.findall(combinedPattern, input)
+
+    multiplicationSum = 0
+    currentlyEnabled = True
+    for expression in expressionList:
+        if expression == enableStr:
+            currentlyEnabled = True
+        elif expression == disableStr:
+            currentlyEnabled = False
+        else: # multiplication expression
+            if currentlyEnabled:
+                multiplicationSum += evaluateMulExpression(expression)
+
+    return multiplicationSum
+
+def evaluateMulExpression(expression):
     numberStrings = re.findall(r"[0-9]+", expression)
     a = int(numberStrings[0])
     b = int(numberStrings[1])
