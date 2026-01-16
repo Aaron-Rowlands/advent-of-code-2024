544722f62ad92cb9d95ca05232633e0bd9f6248a
Aaron Rowlands <aaron-s@gmx.net>
2026-01-16 17:29:53 +0800

    Reformat python file

--- day2-test.py
+++ day2-test.py
@@ -3,9 +3,9 @@ import day2 as subject
 
 class testDay2(unittest.TestCase):
     def test_unsafe_report(self):
-        report = [9,10,18]
+        report = [9, 10, 18]
         self.assertFalse(subject.isSafe(report))
 
     def test_safe_report(self):
-        report = [10,7,4,2,1]
+        report = [10, 7, 4, 2, 1]
         self.assertTrue(subject.isSafe(report))
